ce826ae5143645ca9c3276218999c51152088fcf
Ankit Sonkusare <ankit@localhost.localdomain>
2019-01-29 00:47:24 +0530

    changes

--- seed/course-seeder.js
+++ seed/course-seeder.js
@@ -7,54 +7,54 @@ var products = [
     imagePath:
       'https://i.ibb.co/1ZXft4V/mehendi1.png',
     title:
-      'SGK Products, Neem Oil Cake (Powder), 100% Organic Fertilizer, 2Kg Bag ',
+      'HENNA MEHNDI FOR BEGINNERS - TYPES OF STRIPS | BEST ONLINE MEHENDI LEARNING TUTORIAL',
     description:
-      'Neem Oil Cake organic manure is the by-product obtained in the process of cold pressing of Neem tree fruits and kernels. It doesnt create "death zones" as other insecticides can. Rich micronutrient content such as Nitrogen, Potassium, Sulphur, Magnesium, Phosphorus, Zinc, Calcium, Copper, Iron and Manganese can enhance the plant immunity and the growth of high yield crops.',
+      'Online Mehndi Learning Videos, New Henna Mehndi Tutorials, Types of Strip Designs in Mehndi, Henna Mehndi for Beginners, How to make strip designs in Mehendi, Beautiful Mehndi Strip Designs, Learn Mehndi Online for Beginners, Types of Strips Henna Video, Learn Various Henna Strip Patterns, Learn Mehndi Strip designs, Best Mehendi Strips Designs, Beginner Mehandi',
     price: 429
   }),
   new Course({
     imagePath:
       'https://i.ibb.co/5sSMZPp/mehendi2.png',
     title:
-      'SGK Products, Neem Oil Cake (Powder), 100% Organic Fertilizer, 2Kg Bag ',
+      'How to learn Mehndi for Beginners ',
     description:
-      'Neem Oil Cake organic manure is the by-product obtained in the process of cold pressing of Neem tree fruits and kernels. It doesnt create "death zones" as other insecticides can. Rich micronutrient content such as Nitrogen, Potassium, Sulphur, Magnesium, Phosphorus, Zinc, Calcium, Copper, Iron and Manganese can enhance the plant immunity and the growth of high yield crops.',
+      'We are going to learn about some basic pattern of mehndi design. Initially, we use the pencil to make these pattern.',
     price: 429
   }),
   new Course({
     imagePath:
       'https://i.ibb.co/h7dRWLp/Data-entry.png',
     title:
-      'SGK Products, Neem Oil Cake (Powder), 100% Organic Fertilizer, 2Kg Bag ',
+      'Data Entry Tutorial In Hindi ',
     description:
-      'Neem Oil Cake organic manure is the by-product obtained in the process of cold pressing of Neem tree fruits and kernels. It doesnt create "death zones" as other insecticides can. Rich micronutrient content such as Nitrogen, Potassium, Sulphur, Magnesium, Phosphorus, Zinc, Calcium, Copper, Iron and Manganese can enhance the plant immunity and the growth of high yield crops.',
-    price: 429
-  }),
-  new Course({
-    imagePath:
-      'https://i.ibb.co/NNz8Qs1/deafness-sign-language.png',
-    title:
-      'SGK Products, Neem Oil Cake (Powder), 100% Organic Fertilizer, 2Kg Bag ',
-    description:
-      'Neem Oil Cake organic manure is the by-product obtained in the process of cold pressing of Neem tree fruits and kernels. It doesnt create "death zones" as other insecticides can. Rich micronutrient content such as Nitrogen, Potassium, Sulphur, Magnesium, Phosphorus, Zinc, Calcium, Copper, Iron and Manganese can enhance the plant immunity and the growth of high yield crops.',
+      'A data entry similar to a typist, is a member of staff employed to enter and update data into a computer system database, often from paper documents using a keyboard, optical scanner, or data recorder.The keyboards used can often have specialist keys and multiple colors to help in the task and speed up the work. ',
     price: 429
   }),
   new Course({
     imagePath:
       'https://i.ibb.co/1ZpqPp6/java-in-hindi.png',
     title:
-      'SGK Products, Neem Oil Cake (Powder), 100% Organic Fertilizer, 2Kg Bag ',
+      'Introduction to Java in Hindi ',
     description:
-      'Neem Oil Cake organic manure is the by-product obtained in the process of cold pressing of Neem tree fruits and kernels. It doesnt create "death zones" as other insecticides can. Rich micronutrient content such as Nitrogen, Potassium, Sulphur, Magnesium, Phosphorus, Zinc, Calcium, Copper, Iron and Manganese can enhance the plant immunity and the growth of high yield crops.',
+      'Java is a programming language that produces software for multiple platforms. When a programmer writes a Java application, the compiled code (known as bytecode) runs on most operating systems (OS), including Windows, Linux and Mac OS. Java derives much of its syntax from the C and C++ programming languages.',
+    price: 429
+  }),
+  new Course({
+    imagePath:
+      ' https://i.ibb.co/NNz8Qs1/deafness-sign-language.png',
+    title:
+      'Deafness and Disability - Sign Language Tutorial ',
+    description:
+      'Deaf Hearing (one finger, not two- whoops!) DeafenedHard of Hearing Hearing AidsLipread (Directional Sign)Sign Language Interpreter Lipspeaker Electronic Notetaker Palantypist Videophone Disabled Blind Usher Syndrome',
     price: 429
   }),
   new Course({
     imagePath:
       'https://i.ibb.co/M91k9nC/learn-sign-language.png',
     title:
-      'SGK Products, Neem Oil Cake (Powder), 100% Organic Fertilizer, 2Kg Bag ',
+      'Learn Indian Sign language "BASIC 25 WORDS"',
     description:
-      'Neem Oil Cake organic manure is the by-product obtained in the process of cold pressing of Neem tree fruits and kernels. It doesnt create "death zones" as other insecticides can. Rich micronutrient content such as Nitrogen, Potassium, Sulphur, Magnesium, Phosphorus, Zinc, Calcium, Copper, Iron and Manganese can enhance the plant immunity and the growth of high yield crops.',
+      'Mr. Arun C. Rao is once again here to teach you the the basic 25 Golden Words. This is an important skill that is completely ignored these days, try to learn few of them and try to communicate with people for whom sign is the only form of communication. We are sure you would feel happy & making them a part of the league gifted with voice.',
     price: 429
   })
 ];
